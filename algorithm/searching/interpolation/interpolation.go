947cb806e06dbd32919d9e0ecbcdbc6eda0383e1
jayvib <jayson.vibandor@gmail.com>
2019-11-17 08:40:20 +0800

    added more files

---
+++ algorithm/searching/interpolation/interpolation.go
@@ -0,0 +1,55 @@
+package interpolation
+
+// nums should be sorted.
+func Search(nums []int, num int) bool {
+	var mid, low, high int
+	low, high = 0, len(nums)-1
+
+	for nums[low] < num && nums[high] > num {
+		// calculate mid using this formula
+		mid = low + ((num-nums[low])*(high-low))/(nums[high]-nums[low])
+
+		if nums[mid] < num {
+			low = mid+1
+		} else if nums[mid] > num {
+			high = mid-1
+		} else {
+			return true
+		}
+	}
+
+	if nums[low] == num {
+		return true
+	} else if nums[high] == num {
+		return true
+	}
+	return false
+}
+
+func SearchReview(nums []int, num int) bool {
+	var low, mid, high int
+	low, high = 0, len(nums)-1
+
+	for nums[low] < num && nums[high] > num {
+		// calculate the mid
+		mid = low + ((num-nums[low])*(high-low))/(nums[high]-nums[low])
+
+		if nums[mid] < num {
+			low = mid+1
+		} else if nums[mid] > num { // find in lower partition
+			high = mid-1
+		} else {
+			return true
+		}
+	}
+
+	if nums[low] == num {
+		return true
+	} else if nums[high] == num {
+		return true
+	}
+	return false
+}
+
+
+
