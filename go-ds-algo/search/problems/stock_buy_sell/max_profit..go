5554e6e11f512045f9ae9e9ed8cd8b95f908a392
jaysonv <danv@itquoter.com>
2020-08-22 08:32:01 +0800

    Added more files

--- go-ds-algo/search/problems/stock_buy_sell/max_profit..go
+++ go-ds-algo/search/problems/stock_buy_sell/max_profit..go
@@ -18,19 +18,32 @@ func MaxProfit(price []int, start, end int) int {
 	for i := start; i < end; i++ {
 
 		// The day at which the stock must be sold
-		for j := i+1; j <= end; j++ {
+		for j := i + 1; j <= end; j++ {
 
 			// If buying the stock at ith day and
 			// selling it at jth day is profitable
 			if price[j] > price[i] {
 				currentProfit := price[j] - price[i] +
-					               MaxProfit(price, start, i-1) +
-												 MaxProfit(price, j+1, end)
-
+					MaxProfit(price, start, i-1) + // Calculate the previous profit from the starting day up to this day
+					MaxProfit(price, j+1, end) // Calculate the profit from this day and onward
 				profit = int(math.Max(float64(profit), float64(currentProfit)))
 			}
 		}
 	}
 
+	return profit
+}
+
+func maxProfit(price []int, start, end int) int {
+	// Base case: Can't calculate the price of the
+	// stock that haven't been bought
+	//if end <= start {
+	//	return 0
+	//}
+
+	var profit int
+	//for i := start; i < end; i++ {
+
+	//}
 	return profit
 }
